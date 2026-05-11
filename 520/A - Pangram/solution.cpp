30d0d387a44d1d97b347a5b24cd8ce9919f69257
Ayushgirigoswami <141568757+Ayushgirigoswami@users.noreply.github.com>
2026-05-11 12:28:54 +0530

    Add Pangram [A] from Codeforces

---
+++ 520/A - Pangram/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin >> n;
+ 
+    string s;
+    cin >> s;
+ 
+    set<char> st;
+ 
+    for(char c : s){
+        st.insert(tolower(c)); // handle uppercase
+    }
+ 
+    if(st.size() == 26)
+        cout << "YES
+";
+    else
+        cout << "NO
+";
+ 
+    return 0;
+}
