fcceca67e6c08df452d211feb8b15f06ffa6e4d4
Ayushgirigoswami <141568757+Ayushgirigoswami@users.noreply.github.com>
2026-05-11 12:30:22 +0530

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int x;
+    cin >> x;
+    int step = (x + 4) / 5;
+    
+cout << step;
+ 
+    return 0;
+}
