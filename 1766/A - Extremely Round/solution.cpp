08812db9d0cc7905df6504ded99c3964e096dcab
Ayushgirigoswami <141568757+Ayushgirigoswami@users.noreply.github.com>
2026-05-11 12:29:14 +0530

    Add Extremely Round [A] from Codeforces

---
+++ 1766/A - Extremely Round/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--){
+ 
+        string s;
+        cin >> s;
+ 
+        int len = s.size();
+ 
+        int first =  s[0]-'0';
+ 
+       
+         cout << (len - 1) * 9 + first << "
+";
+        
+         }
+ 
+    return 0;
+}
