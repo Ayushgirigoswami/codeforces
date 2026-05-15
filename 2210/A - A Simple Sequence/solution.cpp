9037dc2cda574e4ca9243a3fac20f8b263eee8ef
Ayushgirigoswami <141568757+Ayushgirigoswami@users.noreply.github.com>
2026-05-15 23:01:57 +0530

    Add A Simple Sequence [A] from Codeforces

--- 2210/A - A Simple Sequence/solution.cpp
+++ 2210/A - A Simple Sequence/solution.cpp
@@ -5,38 +5,38 @@
 // Time Limit: 1000 ms
 // 
 // Powered by CP Editor (https://cpeditor.org)
- 
+ 
 /**
  *  Author: Ayushgiri
 **/
- 
+ 
 #include <bits/stdc++.h>
 using namespace std;
- 
+ 
 using ll = long long;
- 
+ 
 void solve() {
 	 int n;
         cin >> n;
- 
+ 
         for (int i = n; i >= 1; i--) {
             cout << i << " ";
         }
- 
+ 
         cout << '
 ';
 }
- 
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+ 
     int t;
     cin >> t;
- 
+ 
     while(t--) {
         solve();
     }
- 
+ 
     return 0;
 }
