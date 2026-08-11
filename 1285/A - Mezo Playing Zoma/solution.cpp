3874e148d8970e363e9caefe2b5579ec86953535
Ayushgirigoswami <141568757+Ayushgirigoswami@users.noreply.github.com>
2026-08-11 16:35:43 +0530

    Add Mezo Playing Zoma [A] from Codeforces

---
+++ 1285/A - Mezo Playing Zoma/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	ios :: sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+ 
+	
+	
+	    
+	    int n;
+	    cin >> n ;
+	    
+	    
+	    string s;
+	    cin >> s;
+	    
+	    cout << n + 1 <<"
+";
+	    
+	
+    
+    return 0;
+}
+ 
